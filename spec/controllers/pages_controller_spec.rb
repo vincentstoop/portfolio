d813873b381e46bee2f548f43460e2f388772393
Vincent Stoop <vincentstoop@gmail.com>
2017-04-03 17:02:51 +0200

    Add test and implement PagesController

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    let(:admin) { create :admin}
+    let(:page) { create(:page, admin: admin, portfolio_item: true) }
+    let(:page2) { create(:page, admin: admin, portfolio_item: true) }
+    let(:page3) { create(:page, admin: admin) }
+    let(:page4) { create(:page, admin: admin) }
+
+    it "shows the index page" do
       get :index
       expect(response).to have_http_status(:success)
+      expect(assigns(:portfolio_items)).to match_array([page, page2])
+      expect(assigns(:pages)).to match_array([page4, page3])
     end
   end
 
