14d3eed9beb69bd2faa124b808c1960471e4e280
Vincent Stoop <vincentstoop@gmail.com>
2017-04-03 15:33:46 +0200

    Add tests and Implement Admin::PagesController

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,22 +1,48 @@
 class Admin::PagesController < ApplicationController
   def index
+    @pages = Page.all
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      flash[:notice] = "Pagina is opgeslagen."
+      redirect_to admin_pages_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
+
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Paginawijziging is opgeslagen."
+      redirect_to admin_page_path(@page)
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :body, :portfolio_item, :admin_id)
+    end
 end
--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -1,32 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Admin::PagesController, type: :controller do
+  let(:page) { create(:page, title: 'Test PagesController') }
+  let(:admin) { create(:admin) }
 
   describe "GET #index" do
-    it "returns http success" do
+    it "shows the index page" do
       get :index
       expect(response).to have_http_status(:success)
+      expect(assigns(:pages)).to eq([page])
     end
   end
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show, params: { id: 1 }
+    it "shows a single page" do
+      get :show, params: { id: page.id }
       expect(response).to have_http_status(:success)
+      expect(assigns(:page).title).to eq("Test PagesController")
     end
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    it "shows the new Page page" do
       get :new
       expect(response).to have_http_status(:success)
+      expect(assigns(:page)).to be_a_new(Page)
     end
   end
 
   describe "GET #edit" do
-    it "returns http success" do
-      get :edit, params: { id: 1 }
+    it "show the edit Page page" do
+      get :edit, params: { id: page.id }
       expect(response).to have_http_status(:success)
+      expect(assigns(:page).title).to eq('Test PagesController')
+      expect(assigns(:page).title).not_to eq('Update PagesController')
+    end
+  end
+
+  describe "POST #create" do
+    it "creates a page" do
+      post :create, params: { page: { title: "Page creation", body: "This is a test body", portfolio_item: true, admin_id: admin.id } }
+      expect(response).to redirect_to(admin_pages_path)
+      expect(flash[:notice]).not_to be_empty
+      expect(assigns(:page).title).to eq("Page creation")
+    end
+  end
+
+  describe "PUT #update" do
+    it "updates a page" do
+      put :update, params: { id: page.id, page: { title: 'Update PagesController' } }
+      expect(response).to redirect_to(admin_page_path(page))
+      expect(flash[:notice]).not_to be_empty
+      expect(assigns(:page).title).to eq('Update PagesController')
     end
   end
 
