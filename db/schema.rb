72aafa023ea2ba9391fd890d52afc2bc6f3e3b74
Vincent Stoop <vincentstoop@gmail.com>
2017-04-03 13:51:34 +0200

    Remove image column from Page model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402160802) do
+ActiveRecord::Schema.define(version: 20170403114948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170402160802) do
   create_table "pages", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.string "image"
     t.boolean "portfolio_item"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
