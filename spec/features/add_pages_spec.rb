4c7961a2673e4f3b1c43acfecb8e1b8dbc3b29e0
Vincent Stoop <vincentstoop@gmail.com>
2017-04-04 14:56:11 +0200

    Add Page feature spec

--- spec/features/add_pages_spec.rb
+++ spec/features/add_pages_spec.rb
@@ -2,36 +2,38 @@ require 'rails_helper'
 
 RSpec.feature "AddPages", type: :feature do
   let(:admin) { create(:admin) }
-  # before(:each) do
-  #   log_in_as(admin)
-  # end
 
   describe "GET /admin/pages/new" do
-    it "redirects to sign_in page when not logged in" do
-      visit new_admin_page_path
-      expect(current_path).to eq(new_admin_session_path)
-      expect(page).to have_text("Log in")
-      expect(page).not_to have_text("Add Page")
-      fill_in "admin_email", with: admin.email
-      fill_in "admin_password", with: admin.password
-      click_button("Log in")
-      expect(current_path).to eq(new_admin_page_path)
-      expect(page).to have_text("Add Page")
-      expect(page.title).to eq("Add Page")
+
+    context "when not logged in" do
+
+      it "redirects to sign_in page" do
+        visit new_admin_page_path
+        expect(current_path).to eq(new_admin_session_path)
+        expect(page).to have_text("Log in")
+        expect(page).not_to have_text("Add Page")
+      end
+
     end
 
-    it "lets you create a new page when logged in" do
-      log_in_as(admin)
-      visit new_admin_page_path
-      expect(current_path).to eq(new_admin_page_path)
-      expect(page).to have_text("Add Page")
-      expect(page.title).to eq("Add Page")
-      fill_in "page_title", with: "Portfolio Item"
-      fill_in "page_body", with: "This is a portfolio item."
-      check("page_portfolio_item")
-      click_button("Save")
-      expect(page).to have_text("Page saved.")
-      expect(page).to have_text("Portfolio Item")
+    context "when logged in" do
+
+      before(:each) { log_in_as(admin) }
+
+      it "lets you create a new page" do
+        visit new_admin_page_path
+        expect(page).to have_text("Add Page")
+        expect(page.title).to eq("Add Page")
+        fill_in "page_title", with: "Portfolio Item"
+        fill_in "page_body", with: "This is a portfolio item."
+        check("page_portfolio_item")
+        click_button("Save")
+        expect(page).to have_text("Page saved.")
+        expect(page).to have_text("Portfolio Item")
+        expect(page.title).to include("Portfolio Item")
+      end
+
     end
+
   end
 end
