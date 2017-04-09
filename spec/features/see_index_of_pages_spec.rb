4a2ba7ba73be26012ea1a5065a6dae3c30979a47
Vincent Stoop <vincentstoop@gmail.com>
2017-04-09 21:49:18 +0200

    Add tests to index page and implement

--- spec/features/see_index_of_pages_spec.rb
+++ spec/features/see_index_of_pages_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature "SeeIndexOfPages", type: :feature do
   let!(:page2) { create(:page, admin: admin, title: "Page 2") }
   let!(:page3) { create(:page, admin: admin, title: "Page 3",
                               portfolio_item: true) }
+  let(:pages) { [page1, page2, page3] }
 
   describe "GET admin/pages" do
 
@@ -21,17 +22,30 @@ RSpec.feature "SeeIndexOfPages", type: :feature do
     end
 
     context "when logged in" do
-      before(:each) { log_in_as(admin) }
-
-      it "shows you the Pages index" do
+      before(:each) {
+        log_in_as(admin)
         visit admin_pages_path
+      }
+
+      it "shows you the Pages index, with links to those pages" do
         expect(current_path).to eq(admin_pages_path)
         expect(page.title).to include("All Pages")
-        expect(page).to have_text("Page 1")
-        expect(page).to have_text("Page 2")
-        expect(page).to have_text("Page 3")
+        pages.each do |p|
+          expect(page).to have_link(p.title, href: admin_page_path(p))
+        end
       end
 
+      it "has links to edit the different Pages" do
+        pages.each do |p|
+          expect(page).to have_link("Edit", href: edit_admin_page_path(p))
+        end
+      end
+
+      it "has links to delete the different Pages" do
+        pages.each do |p|
+          expect(page).to have_link("Delete", href: admin_page_path(p))
+        end
+      end
     end
 
   end
