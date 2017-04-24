72e30897be9d8b0a9dcaf2a3e4d8893057159f92
Vincent Stoop <vincentstoop@gmail.com>
2017-04-24 16:21:29 +0200

    Add admin views for portfolio items

--- app/controllers/admin/portfolio_items_controller.rb
+++ app/controllers/admin/portfolio_items_controller.rb
@@ -1,6 +1,18 @@
 class Admin::PortfolioItemsController < Admin::BaseController
+  before_action :set_portfolio_item
 
   def index
     @portfolio_items = PortfolioItem.all
   end
+
+  def show
+  end
+
+
+
+  private
+
+    def set_portfolio_item
+      @portfolio_item = PortfolioItem.find(params[:id])
+    end
 end
